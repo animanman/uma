dd275ca03c6d2c1d32a8791d7be141ab33269910
animanman <103096854+animanman@users.noreply.github.com>
2022-08-09 16:29:39 +0900

    Create ped.js

---
+++ ped.js
@@ -0,0 +1,382 @@
+,
+        horse: {
+           stallions: [
+//サンデー父系10
+           {horsename: 'サンデーサイレンス',
+            id: 700001,
+            parentped: {
+              male: 700002,
+              //female: 3
+             }
+            },
+           {horsename: 'Halo',
+            id: 700002,
+            parentped: {
+              male: 700003,
+              //female: 3
+             }
+            },
+           {horsename: 'Hail to Reason',
+            id: 700003,
+            parentped: {
+              male: 700004,
+              //female: 3
+             }
+            },
+           {horsename: 'Turn-to',
+            id: 700004,
+            parentped: {
+              male: 700005,
+              //female: 3
+             }
+            },
+           {horsename: 'Royal Charger',
+            id: 700005,
+            parentped: {
+              male: 700006,
+              //female: 3
+             }
+            },
+           {horsename: 'Nearco',
+            id: 700006,
+            parentped: {
+              male: 700007,
+              //female: 3
+             }
+            },
+           {horsename: 'Pharos',
+            id: 700007,
+            parentped: {
+              male: 700008,
+              //female: 3
+             }
+            },
+           {horsename: 'Phalaris',
+            id: 700008,
+            parentped: {
+              male: 700009,
+              //female: 3
+             }
+            },
+           {horsename: 'Polymelus',
+            id: 700009,
+            parentped: {
+              male: 700010,
+              //female: 3
+             }
+            },
+           {horsename: 'Cyllene',
+            id: 700010
+            },
+//サンデー母父系10
+           {horsename: 'Understanding',
+            id: 700011,
+            parentped: {
+              male: 700012,
+              //female: 3
+             }
+            },
+           {horsename: 'Promised Land',
+            id: 700012,
+            parentped: {
+              male: 700013,
+              //female: 3
+             }
+            },
+           {horsename: 'Palestinian',
+            id: 700013,
+            parentped: {
+              male: 700014,
+              //female: 3
+             }
+            },
+           {horsename: 'Sun Again',
+            id: 700014,
+            parentped: {
+              male: 700015,
+              //female: 3
+             }
+            },
+           {horsename: 'Sun Teddy',
+            id: 700015,
+            parentped: {
+              male: 700016,
+              //female: 3
+             }
+            },
+           {horsename: 'Teddy',
+            id: 700016,
+            parentped: {
+              male: 700017,
+              //female: 3
+             }
+            },
+           {horsename: 'Ajax',
+            id: 700017,
+            parentped: {
+              male: 700018,
+              //female: 3
+             }
+            },
+           {horsename: 'Flying Fox',
+            id: 700018,
+            parentped: {
+              male: 700019,
+              //female: 3
+             }
+            },
+           {horsename: 'Orme',
+            id: 700019,
+            parentped: {
+              male: 700020,
+              //female: 3
+             }
+            },
+           {horsename: 'Ormonde',
+            id: 700020
+            },
+//サンデー父母父系
+           {horsename: 'Cosmic Bomb',
+            id: 700021,
+            parentped: {
+              male: 700022,
+              //female: 3
+             }
+            },
+           {horsename: 'Pharamond',
+            id: 700022,
+            parentped: {
+              male: 700008,
+              //female: 3
+             }
+            },
+//サンデー母母父系8
+           {horsename: 'Montparnasse',
+            id: 700023,
+            parentped: {
+              male: 700024,
+              //female: 3
+             }
+            },
+           {horsename: 'Gulf Stream',
+            id: 700024,
+            parentped: {
+              male: 700025,
+              //female: 3
+             }
+            },
+           {horsename: 'Hyperion',
+            id: 700025,
+            parentped: {
+              male: 700026,
+              //female: 3
+             }
+            },
+           {horsename: 'Gainsborough',
+            id: 700026,
+            parentped: {
+              male: 700027,
+              //female: 3
+             }
+            },
+           {horsename: 'Bayardo',
+            id: 700027,
+            parentped: {
+              male: 700028,
+              //female: 3
+             }
+            },
+           {horsename: 'Bay Ronald',
+            id: 700028,
+            parentped: {
+              male: 700029,
+              //female: 3
+             }
+            },
+           {horsename: 'Hampton',
+            id: 700029,
+            parentped: {
+              male: 700030,
+              //female: 3
+             }
+            },
+           {horsename: 'Lord Clifden',
+            id: 700030
+            },
+//サンデー父父母父系8
+           {horsename: 'Blue Swords',
+            id: 700031,
+            parentped: {
+              male: 700032,
+              //female: 3
+             }
+            },
+           {horsename: 'Blue Larkspur',
+            id: 700032,
+            parentped: {
+              male: 700033,
+              //female: 3
+             }
+            },
+           {horsename: 'Black Servant',
+            id: 700033,
+            parentped: {
+              male: 700034,
+              //female: 3
+             }
+            },
+           {horsename: 'Black Toney',
+            id: 700034,
+            parentped: {
+              male: 700035,
+              //female: 3
+             }
+            },
+           {horsename: 'Peter Pan',
+            id: 700035,
+            parentped: {
+              male: 700036,
+              //female: 3
+             }
+            },
+           {horsename: 'Commando',
+            id: 700036,
+            parentped: {
+              male: 700037,
+              //female: 3
+             }
+            },
+           {horsename: 'Domino',
+            id: 700037,
+            parentped: {
+              male: 700038,
+              //female: 3
+             }
+            },
+           {horsename: 'Himyar',
+            id: 700038
+            },
+//サンデー父母母父系8
+           {horsename: 'Mahmoud',
+            id: 700039,
+            parentped: {
+              male: 700040,
+              //female: 3
+             }
+            },
+           {horsename: 'Blenheim',
+            id: 700040,
+            parentped: {
+              male: 700041,
+              //female: 3
+             }
+            },
+           {horsename: 'Blandford',
+            id: 700041,
+            parentped: {
+              male: 700042,
+              //female: 3
+             }
+            },
+           {horsename: 'Swynford',
+            id: 700042,
+            parentped: {
+              male: 700043,
+              //female: 3
+             }
+            },
+           {horsename: 'John o'Gaunt',
+            id: 700043,
+            parentped: {
+              male: 700044,
+              //female: 3
+             }
+            },
+           {horsename: 'Isinglass',
+            id: 700044,
+            parentped: {
+              male: 700045,
+              //female: 3
+             }
+            },
+           {horsename: 'Isonomy',
+            id: 700045,
+            parentped: {
+              male: 700046,
+              //female: 3
+             }
+            },
+           {horsename: 'Sterling',
+            id: 700046
+            }
+           ]
+           broodmares: [
+//スペちゃん母系
+           {horsename: 'キャンペーンガール',
+            id: 300001,
+            parentped: {
+              male: 0004,
+              female: 300002
+             }
+            },
+           {horsename: 'レディーシラオキ',
+            id: 300002,
+            parentped: {
+              //male:セントクレスピン,
+              female: 300003
+             }
+            },
+           {horsename: 'ミスアシヤガワ',
+            id: 300003,
+            parentped: {
+              //male:ヒンドスタン,
+              female: 300004
+             }
+            },
+           {horsename: 'シラオキ',
+            id: 300004,
+            parentped: {
+              //male:プリメロ,
+              female: 300005
+             }
+            },
+           {horsename: '第弐スターカツプ',
+            id: 300005,
+            parentped: {
+              //male:ダイオライト,
+              female: 300006
+             }
+            },
+           {horsename: 'スターカツプ',
+            id: 300006,
+            parentped: {
+              //male:シアンモア,
+              female: 300007
+             }
+            },
+           {horsename: 'フロリスト',
+            id: 300007,
+            parentped: {
+              //male:ガロン,
+              female: 300008
+             }
+            },
+           {horsename: '第四フロリースカツプ',
+            id: 300008,
+            parentped: {
+              //male:インタグリオー,
+              female: 300009
+             }
+            },
+           {horsename: 'フロリースカツプ',
+            id: 300009,
+            parentped: {
+              //male:Florizel,
+              female: 300010
+             }
+            },
+           {horsename: 'Stirrup Cup',
+            id: 300010
+            }
+           ]
+        }
